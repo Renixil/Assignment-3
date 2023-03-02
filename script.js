eef5da24ea73a3f98bc028265d71b272b09c86de
Renixil <125784936+Renixil@users.noreply.github.com>
2023-03-01 21:05:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-    function startTimer() { // Alert Message
-      alert("Prepare For Blast Off!");
-    }
 var timerId; // stores reference to the timeout
 
 function startTimer() {
+  alert("Prepare For Blast Off!");
   var timeLeft = 10;
   const timerEl = document.getElementById("DisplayCountdown"); // ID tag h2
-      alert("Prepare For Blast Off!");
 
   const countDown = function() {
-    timeLeft--; //
-    timerEl.innerHTML= timeLeft; // Lowers the number by one
-
+    timeLeft--; //Decrement the timeLeft by 1
+    timerEl.innerHTML= timeLeft;
     if (timeLeft === 5) {
-      alert("Warning Less than ½ way to launch, time left: " + timeLeft + " seconds"); // Alert message with variable
-      timerEl.innerHTML= timeLeft; // Lowers the number by one
-        timeLeft +4; // Without this variable and integer, the countdown will skip the number 3 and move from 4->2 instead.
-        timerId = setTimeout(countDown, 1000) // this will repeat the decrementing function of the countdown
+      alert("Warning Less than ½ way to launch, time left: " + timeLeft + " seconds"); // Alert message with String concatenation
+      timerId = setTimeout(countDown, 1000) // this will repeat the decrementing function of the countdown
     }
     else if (timeLeft === 0) {
-      timerEl.innerHTML= "Blast Off!"; // This change the text of h2 into 'BlastOff' when the timer hit 
-      console.log('Blast Off!')
+      timerEl.innerHTML= "Blast Off!"; // This change the text of h2 into 'BlastOff' when the timer hit
+      alert("Blast Off!"); // Alert message
     } else {
       timerId = setTimeout(countDown, 1000); //setTimeout is a function that takes 2 arguments, 1 argument is a function, and 2nd is time or miliseconds. 1000ms = 1second. It will call the function every second. This will run the function indefinetly. This also store to timerId for a reference to stop timer.
+
     }
+
   };
 
   countDown(); // This starts the countdown after button clicked.
